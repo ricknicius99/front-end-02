3077adc2b1857a3bca2f38268122df520ed04134
vinicius machado rodrigues <viniciusmr99@gmail.com>
2022-03-07 22:49:18 -0300

    atividades de realizacao de ligacao de <script>

---
+++ aula-01/main.js
@@ -0,0 +1,7 @@
+const numeros = [1, 3, 5, 8, 12, 42, 96, 1024];
+
+let somaNumeros = 0;
+for(let i = 0; i < numeros.length; i++){
+    somaNumeros = somaNumeros + numeros[i];
+    console.log(somaNumeros)
+};
